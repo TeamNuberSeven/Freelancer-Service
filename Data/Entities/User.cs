baa87c4047f522fef1704342f182f1f1a3853454
LyubomurBuryi <gruf.bird@gmail.com>
2019-03-24 19:46:15 +0200

    add data layer

---
+++ Data/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Data.Entities
+{
+    public class User
+    {
+        public String Email { get; set; }
+        public String Name { get; set; }
+    }
+}
---
+++ Data/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options) {  }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
