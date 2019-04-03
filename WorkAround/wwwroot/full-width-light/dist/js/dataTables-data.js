0d0fe78a53bcc97489b1559b1e2062c13083a381
maksgr <maksgribun@gmail.com>
2019-04-03 19:30:16 +0300

    Add login view

---
+++ WorkAround/wwwroot/full-width-light/dist/js/dataTables-data.js
@@ -0,0 +1,12 @@
+/*DataTable Init*/
+
+"use strict"; 
+
+$(document).ready(function() {
+	"use strict";
+	
+	$('#datable_1').DataTable();
+	$('#datable_3').DataTable();
+	$('#datable_4').DataTable();
+    $('#datable_2').DataTable({ "lengthChange": false});
+} );
