bd932101bcf6ec9afd370a43e5c00b2f55b816e5
RudenkoOleksii <oleksii98@gmail.com>
2019-04-03 20:27:52 +0300

    change startup.cs

--- WorkAround/Startup.cs
+++ WorkAround/Startup.cs
@@ -35,10 +35,13 @@ namespace WorkAround
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<UserContext>(options => options.UseSqlServer(
-                                                    Configuration.GetConnectionString("LibraryConnection")
-                                                    )
-                                                );
+            services.AddDbContext<UserContext>(options =>
+            {
+              options.UseSqlServer(
+                  Configuration.GetConnectionString("DefaultConnection")
+              );
+            }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
